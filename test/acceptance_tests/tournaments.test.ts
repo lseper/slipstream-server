de176a35f2be988e9056a431b47e6ca3e00e5f70
= <seperliam@gmail.com>
2023-04-22 13:25:00 -0500

    initial testing

---
+++ test/acceptance_tests/tournaments.test.ts
@@ -0,0 +1,13 @@
+import * as request from "supertest";
+
+describe("GET /api/v1/tournaments", () => {
+	it("SHOULD return 200Ok", (done) => {
+		// TODO: need to setup mongoDB instance locally with Docker for mock DB testing
+		// request(app)
+		// 	.get("/articles")
+		// 	.end((err, res) => {
+		// 		expect(res.status).toBe(200);
+		// 		done();
+		// 	});
+	});
+});
